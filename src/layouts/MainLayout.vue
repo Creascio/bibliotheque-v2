0105b5a40d05740a76af9bbd1c8b3eb42a4566c1
Shiropetite <59747311+Shiropetite@users.noreply.github.com>
2021-07-05 23:20:56 +0200

    troubleshooting done

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -13,6 +13,39 @@
     </q-header>
     <q-page-container>
       <router-view />
+      <q-btn-dropdown
+        dropdown-icon="campaign"
+        rounded
+        dense
+        color="primary"
+        size="xl"
+        class="troubleshooting q-pa-xs"
+        no-icon-animation
+      >
+        <q-card class="bg-grey-5 q-pa-sm" style="width: 295px; height: 140px">
+          <div class="row q-gutter-md justify-center items-end">
+            <q-img
+              src="https://cdn.discordapp.com/attachments/822839106001829908/861662175113773086/clubbo.png"
+              width="70px"
+              height="70px"
+              class="col-4"
+            />
+            <div class="col-7 text-justify">
+              Vous avez une idée de fonctionnalité ? de design ? Vous avez repérez un bug ou une faute de français ?
+              <div class="row q-pt-sm">
+                <q-space />
+                <a
+                  style="text-decoration: none"
+                  target="_blank"
+                  href="https://github.com/Creascio/bibliotheque-v2/issues/new/choose"
+                >
+                  <q-btn dense no-caps flat color="accent">Alertez nous !</q-btn>
+                </a>
+              </div>
+            </div>
+          </div>
+        </q-card>
+      </q-btn-dropdown>
     </q-page-container>
   </q-layout>
 </template>
@@ -25,7 +58,16 @@ export default defineComponent({
   components: {
     SearchBar
   },
-  setup() {}
+  setup() {
+    return { open: false };
+  }
 });
 </script>
+<style>
+.troubleshooting {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+}
+</style>
 
